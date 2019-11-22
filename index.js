76b61d1ec3453f9774c6249fc8cf701e056a6594
Ryan Lambrecht <rlambrecht09@gmail.com>
2019-11-21 20:06:17 -0600

    added api functionality

--- index.js
+++ index.js
@@ -1,12 +1,88 @@
+var express = require("express");
 var DelcomIndicator = require("./DelcomIndicator");
+var bodyParser = require("body-parser");
 
-var delcomIndicator = new DelcomIndicator();
-if (delcomIndicator.isOpen()) {
-  delcomIndicator.solidBlue();
-  setTimeout(function() {
-    delcomIndicator.solidGreen();
-  }, 3000);
-  setTimeout(function() {
-    delcomIndicator.turnOff();
-  }, 3000);
-}
+// Initalize
+const app = express();
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const delcomIndicator = new DelcomIndicator();
+const PORT = 5000;
+
+app.post("/api/v1/lightstate", (req, res) => {
+  if (!delcomIndicator.isConnected) {
+    const delcomIndicator = new DelcomIndicator();
+  }
+  var status = req.body.status;
+  var color = req.body.color;
+  if (!delcomIndicator.isConnected()) {
+    return res.status(500).send({
+      success: "false",
+      message: "no light is connected"
+    });
+  } else if (!status) {
+    return res.status(400).send({
+      success: "false",
+      message: "light status is required"
+    });
+  }
+  // turn off before trying to change
+  delcomIndicator.turnOff();
+  switch (status) {
+    case "off":
+      delcomIndicator.turnOff();
+      break;
+    case "on":
+      switch (req.body.color) {
+        case "red":
+          delcomIndicator.solidRed();
+          break;
+        case "blue":
+        case "yellow":
+          delcomIndicator.solidBlue();
+          break;
+        case "green":
+          delcomIndicator.solidGreen();
+          break;
+        default:
+          return res.status(400).send({
+            success: "false",
+            message: "invalid light color"
+          });
+      }
+      break;
+    case "blink":
+      switch (req.body.color) {
+        case "red":
+          delcomIndicator.flashRed();
+          break;
+        case "blue":
+        case "yellow":
+          delcomIndicator.flashBlue();
+          break;
+        case "green":
+          delcomIndicator.flashGreen();
+          break;
+        default:
+          return res.status(400).send({
+            success: "false",
+            message: "invalid light color"
+          });
+      }
+      break;
+    default:
+      return res.status(400).send({
+        success: "false",
+        message: "invalid light status"
+      });
+  }
+
+  return res.status(200).send({
+    success: "true"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
